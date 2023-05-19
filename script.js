4d21f406c778befe02393954a29d8a6c7c3ca165
ryangabriel27 <ryanjulio56@gmail.com>
2023-05-19 16:50:38 -0300

    RECEBA

---
+++ script.js
@@ -0,0 +1,21 @@
+function cadastroEvento() {
+    let nomeEvento, dataEvento, nParticipantes, nomePalestrante
+
+    nomeEvento = document.getElementById("eNome").value;
+    dataEvento = document.getElementById("eData").value;
+    nParticipantes = document.getElementById("eNumero").value;
+    nomePalestrante = document.getElementById("eNomeP").value;
+
+    if (nomeEvento != "") {
+
+    }
+    if (dataEvento != da) {
+
+    }
+    if (nParticipantes > 0 && nParticipantes <= 100) {
+
+    }
+    if (nomePalestrante != "") {
+
+    }
+}
